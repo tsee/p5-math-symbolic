5630064dda9fcaf0a18894e3bf48402b81375c28
tsee <tsee@4b0c9c90-080e-0410-a236-80636a358760>
2009-11-08 12:33:24 +0000

    copyright update
    git-svn-id: svn+ssh://svn.code.sf.net/p/math-symbolic/code@130 4b0c9c90-080e-0410-a236-80636a358760

--- msc-transformation/lib/Math/Symbolic/Custom/Transformation.pm
+++ msc-transformation/lib/Math/Symbolic/Custom/Transformation.pm
@@ -606,7 +606,7 @@ Steffen Müller, E<lt>symbolic-module at steffen-mueller dot netE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2006-2008 by Steffen Mueller
+Copyright (C) 2006-2009 by Steffen Mueller
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.6.1 or,
