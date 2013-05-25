6a9e5f0ad1a8839b11756961f7ee8e4dc554eb61
tsee <tsee@4b0c9c90-080e-0410-a236-80636a358760>
2013-05-25 15:58:54 +0000

    Add UTF8 POD encoding hint
    git-svn-id: svn+ssh://svn.code.sf.net/p/math-symbolic/code@162 4b0c9c90-080e-0410-a236-80636a358760

--- msc-pattern/lib/Math/Symbolic/Custom/Pattern.pm
+++ msc-pattern/lib/Math/Symbolic/Custom/Pattern.pm
@@ -30,6 +30,7 @@ use constant NAMED_VAR   => 5;
 
 use constant ATTR_COMMUTATIVE => 1;
 
+=encoding utf8
 
 =head1 NAME
 
@@ -540,11 +541,11 @@ enhancing Math::Symbolic.
 
 =head1 AUTHOR
 
-Steffen Müller, E<lt>symbolic-module at steffen-mueller dot netE<gt>
+Steffen Müller, E<lt>smueller@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2005-2009 by Steffen Müller
+Copyright (C) 2005, 2006, 2008, 2009, 2013 by Steffen Mueller
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.4 or,
